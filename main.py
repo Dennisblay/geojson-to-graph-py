c01f9452e269538edda43120c7aed7d00533c729
Dennis Boachie Boateng <dennisboachie9@gmail.com>
2023-11-29 18:47:09 +0000

    LineString

--- main.py
+++ main.py
@@ -3,39 +3,32 @@ from graph.graph import Graph, Node
 from utils.utils import get_weight, is_adjacent, NodeKeyGenerator
 from pprint import pprint
 
-gdf = gdp.read_file("paths_test.geojson")
+gdf = gdp.read_file("paths.geojson")
 
 new_graph = Graph()
-prev_multi_line = None
+prev_coords_pair = None
 node_key_generator = NodeKeyGenerator()
 for index, current_row in gdf.iterrows():
-    for multi_line in current_row.geometry.geoms:
-        print(list(multi_line.coords))
-        curr_coords = list(multi_line.coords)
-        x_from = curr_coords[0][0]  # X1
-        y_from = curr_coords[0][1]  # Y1
+    current_segment = list(current_row.geometry.coords)
+    if len(current_segment) != 0:
+        for (x, y) in current_segment:
+            if prev_coords_pair is not None:
+                from_node = Node(
+                    x=x,
+                    y=y,
+                    label=node_key_generator.generate_node_key(f"{x}-{y}")
+                )
 
-        x_to = curr_coords[1][0]  # X2
-        y_to = curr_coords[1][1]  # Y2
+                x_to, y_to = prev_coords_pair
+                to_node = Node(
+                    x=x_to,
+                    y=y_to,
+                    label=node_key_generator.generate_node_key(f"{x_to}-{y_to}")
+                )
 
-        # if not is_adjacent(previous=prev_multi_line, current=multi_line):
-        #     continue
-
-        from_node = Node(
-            x=x_from,
-            y=y_from,
-            label=node_key_generator.generate_node_key(f"{x_from}-{y_from}")
-        )
-
-        to_node = Node(
-            x=x_to,
-            y=y_to,
-            label=node_key_generator.generate_node_key(f"{x_to}-{y_to}")
-        )
-
-        new_graph.add_node(from_node=from_node, to_node=to_node,
-                           weight=get_weight(from_node=from_node,
-                                             to_node=to_node))
-        prev_multi_line = multi_line
+                new_graph.add_node(from_node=from_node, to_node=to_node,
+                                   weight=get_weight(from_node=from_node,
+                                                     to_node=to_node))
+            prev_coords_pair = x, y
 pprint(new_graph.edges)
 pprint(new_graph.weights)
