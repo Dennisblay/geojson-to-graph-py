1eaea233c8ec91ca2aec12ce59d4c5d8b56774df
Dennis Boachie Boateng <dennisboachie9@gmail.com>
2023-12-01 10:47:24 +0000

    file path integrity

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from dijkstra.dijkstra import dijkstra
 from utils.utils import read_to_graph
+import os
 
-graph = read_to_graph(file_name="paths.geojson")
-distance, shortest_path = dijkstra(graph=graph, initial="n189", end="n167")
+if os.path.exists(path="paths.geojson"):
 
-if shortest_path:
-    graph.nodes_to_csv()
+    graph = read_to_graph(file_name="paths.geojson")
+    distance, shortest_path = dijkstra(graph=graph, initial="n189", end="n167")
 
-    print(f"{distance}m", f"From {shortest_path[0]} To {shortest_path[-1]}")
+    if shortest_path:
+        graph.nodes_to_csv()
+
+        print(f"{distance}m", f"From {shortest_path[0]} To {shortest_path[-1]}")
+
+    else:
+        print("RouteNotPossible")
+
+    distance, closest_node = graph.query_closest_location(x=-174200.003671526181279, y=744552.949123957310803,
+                                                          label="park_point")
+    graph.node_to_csv(closest_node)
 
 else:
-    print("RouteNotPossible")
-
-distance, closest_node = graph.query_closest_location(x=-174200.003671526181279, y=744552.949123957310803,
-                                                      label="park_point")
-graph.node_to_csv(closest_node)
+    print("File does not exist, Check file path integrity")
